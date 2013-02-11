57e90fab55dd50c96f19c821dfd559e8561b2438
Roberto Giessler <roberto.giessler@googlemail.com>
2013-02-11 13:40:36 +0100

    ADDED: NSManagedObject+XingAPIAdditions: Temporyry code that returns sample JSON

--- XINGPad/Classes/Model/CoreDataCategories/NSManagedObject+XingAPIAdditions.m
+++ XINGPad/Classes/Model/CoreDataCategories/NSManagedObject+XingAPIAdditions.m
@@ -12,6 +12,30 @@
 @implementation NSManagedObject(XingAPIAdditions)
 + (void)fetchJSONForPath:(NSString *)path withBlock:(void (^)(NSString *, NSError *error))block {
 	
+	// Read placeholder string for testing
+	NSError *error;
+	NSString *jsonPath   = [[NSBundle mainBundle] pathForResource:@"NetworkActivity" ofType:@"json"];
+	NSString *jsonString = [NSString stringWithContentsOfFile:jsonPath encoding:NSUTF8StringEncoding error:&error];
+	
+	// Error
+	if (error) {
+		
+		// Call block with error object
+		block(nil, error);
+		
+	// Success
+	} else if (jsonString) {
+		
+		// Call block with JSON
+		block(jsonString, nil);
+		
+	// Nothing to return
+	} else {
+		
+		// Call block with nil values
+		block (nil, nil);
+	}
+	
 	// Build OAuth Fetching stuff
 	// Get JSON string
 	
--- XINGPad/Classes/ViewControllers/XPHomeViewController/XPHomeViewController.m
+++ XINGPad/Classes/ViewControllers/XPHomeViewController/XPHomeViewController.m
@@ -6,12 +6,25 @@
 // Header
 #import "XPHomeViewController.h"
 
+// App classes
+// Model categories
+#import "XPUser+XPAdditions.h"
+
 @implementation XPHomeViewController
 
 // MARK: Template methods
 - (void)viewDidAppear:(BOOL)animated {	
 	[super viewDidAppear:animated];
 	
-	
+	// Fetch activities for user
+	[XPUser activitiesWithBlock:^(XPActivity *activity, NSError *error) {
+		
+		if (error) {
+			;
+			
+		} else if (activity) {
+			DLog(@"%@", activity);
+		}
+	}];
 }
 @end
